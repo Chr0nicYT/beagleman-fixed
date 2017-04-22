0f3ee935cc40c3be0d5fcab1a80b0d8a5180c46f
WeCreate180 <tanayphone@gmail.com>
2017-04-22 01:05:12 +0000

    First Commit

---
+++ ex_creds.py
@@ -0,0 +1,10 @@
+#RENAME THIS TO creds.py BY DOING: sudo mv ex_creds.py creds.py
+#Enjoy!
+#Fill these lines with the strings on developer.amazon.com
+#I assume you know what you're doing.
+
+ProductID = ""
+Security_Profile_Description = ""
+Security_Profile_ID = ""
+Client_ID = ""
+Client_Secret = ""
